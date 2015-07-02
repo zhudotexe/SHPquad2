01ab2aaf7847ff56a61b9e23ad2028d327cec0b5
Andrew Zhu <mommothazaz123@gmail.com>
2015-07-02 16:34:06 +0800

    Added handler for no connection.

--- QuadTest2/ImageDownloader.m
+++ QuadTest2/ImageDownloader.m
@@ -15,13 +15,20 @@
     self.images = [[NSMutableArray alloc]init];
     NSArray *imageArray = array;
     NSLog(@"Image Array: %@", imageArray);
-    for (int i =0; i < [imageArray count]; i++) {
+    for (int i = 0; i < [imageArray count]; i++) {
         NSLog(@"Downloading Image: %d", i);
         NSURL *imageURL = [imageArray objectAtIndex:i];
         NSLog(@"URL: %@", [imageArray objectAtIndex:i]);
         UIImage *image = [[UIImage alloc]initWithData:[[NSData alloc]initWithContentsOfURL:imageURL]];
         NSLog(@"Downloaded Image");
-        [self.images addObject:image];
+        if (image) {
+            [self.images addObject:image];
+        } else {
+            UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Connection Error" message:@"Error connecting to server, check internet connection and try again." delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alert show];
+            break;
+        }
+        
     }
     
     NSLog(@"Images Downloaded");
