6487706a927dc8b6791297caf05190f7edface02
Andrew Zhu <mommothazaz123@gmail.com>
2015-09-17 14:52:10 -0700

    Added custom segue

---
+++ QuadTest2/LeftToRightSegue.h
@@ -0,0 +1,15 @@
+//
+//  LeftToRightSegue.h
+//  SHPQuad
+//
+//  Created by Andrew Zhu on 9/17/15.
+//  Copyright © 2015 SHPQuad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface LeftToRightSegue : UIStoryboardSegue
+
+@end
---
+++ QuadTest2/LeftToRightSegue.m
@@ -0,0 +1,34 @@
+//
+//  LeftToRightSegue.m
+//  SHPQuad
+//
+//  Created by Andrew Zhu on 9/17/15.
+//  Copyright © 2015 SHPQuad. All rights reserved.
+//
+
+#import "LeftToRightSegue.h"
+
+@implementation LeftToRightSegue
+
+-(void)perform {
+    
+    __block UIViewController *sourceViewController = (UIViewController*)[self sourceViewController];
+    __block UIViewController *destinationController = (UIViewController*)[self destinationViewController];
+    
+    CATransition* transition = [CATransition animation];
+    transition.duration = .25;
+    transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    transition.type = kCATransitionPush; //kCATransitionMoveIn; //, kCATransitionPush, kCATransitionReveal, kCATransitionFade
+    transition.subtype = kCATransitionFromLeft; //kCATransitionFromLeft, kCATransitionFromRight, kCATransitionFromTop, kCATransitionFromBottom
+    
+    
+    
+    [sourceViewController.navigationController.view.layer addAnimation:transition
+                                                                forKey:kCATransition];
+    
+    [sourceViewController.navigationController pushViewController:destinationController animated:NO];
+    
+    
+}
+
+@end
